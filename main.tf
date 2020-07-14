78885c5116e17b35ce35a72119dd2028a69df19b
Vinayak Majagaonkar <vinayaksm@gmail.com>
2020-07-14 16:24:39 +0100

    Changes to org name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "<YOUR-ORG-NAME>"
+    organization = "vinayaksm"
 
     workspaces {
       name = "learn-terraform-circleci"
